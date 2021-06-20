c73c446ea41afb4d0b7296ebeb31e55593ba0fde
Lucas Prazeres <olucascostadp@gmail.com>
2021-06-20 09:05:15 -0300

    chore: add eslint, prettier and editorconfig

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,21 +1,27 @@
-import React from "react";
-import { Image, Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
+import React from 'react';
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from 'react-native';
 
-import discordImg from "../../assets/discord.png";
+import discordImg from '../../assets/discord.png';
 
-import { styles } from "./styles";
+import { styles } from './styles';
 
 type Props = TouchableOpacityProps & {
   children: string;
-}
+};
 
 export function ButtonIcon({ children, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
       <View style={styles.iconWrapper}>
-        <Image source={discordImg} style={styles.icon}/>
+        <Image source={discordImg} style={styles.icon} />
       </View>
       <Text style={styles.title}>{children}</Text>
     </TouchableOpacity>
-  )
+  );
 }
--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,8 +1,8 @@
 export const theme = {
   colors: {
-    background: "#0D133D",
-    heading: "#DDE3F0",
-    primary: "#E51C44",
-    line: "#991F36"
-  }
-}
+    background: '#0D133D',
+    heading: '#DDE3F0',
+    primary: '#E51C44',
+    line: '#991F36',
+  },
+};
